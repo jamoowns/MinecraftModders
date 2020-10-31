91e677e484e3e6e5aa86a937e58b3e18c367ca8b
mabmo <martin.o.94@hotmail.com>
2020-10-31 14:31:25 +1100

    Update MobListener.java

--- src/minecraftmodders/MobListener.java
+++ src/minecraftmodders/MobListener.java
@@ -18,30 +18,34 @@ public class MobListener implements Listener {
 	public MobListener(JavaPlugin aJavaPlugin) {
 		javaPlugin = aJavaPlugin;
 	}
-	
 
     @EventHandler
 	public void onPlayerMoveEvent(PlayerMoveEvent event) {
 		// TODO Auto-generated method stub
-    	BlockFace facing = event.getPlayer().getFacing();
-    	int x = 0;
-		int z = 0;
-		if (facing == BlockFace.SOUTH) {
-    		x = 0;
-    		z = -1;
-		} else if (facing == BlockFace.NORTH) {
-        		x = 0;
-        		z = 1;
-    	} else if (facing == BlockFace.EAST) {
-    		x = -1;
-    		z = 0;
-    	} else if (facing == BlockFace.WEST) {
-    		x = 1;
-    		z = 0;
+    	if(!event.getPlayer().getLocation().add(0, -1, 0).getBlock().isEmpty()) {
+	    	BlockFace facing = event.getPlayer().getFacing();
+	    	int x = 0;
+			int z = 0;
+			if (facing == BlockFace.SOUTH) {
+	    		x = 0;
+	    		z = -1;
+			} else if (facing == BlockFace.NORTH) {
+	        		x = 0;
+	        		z = 1;
+	    	} else if (facing == BlockFace.EAST) {
+	    		x = -1;
+	    		z = 0;
+	    	} else if (facing == BlockFace.WEST) {
+	    		x = 1;
+	    		z = 0;
+	    	}
+	    	Location loc = event.getPlayer().getLocation().add(x, -1, z);
+	    	
+	    	if(event.getPlayer().getName()=="mabmo") {
+	    		event.getPlayer().getWorld().getBlockAt(loc).setType(Material.CYAN_WOOL);
+	    	}else{
+	    		event.getPlayer().getWorld().getBlockAt(loc).setType(Material.RED_WOOL);
+	    	}
     	}
-    	Location loc = event.getPlayer().getLocation().add(x, -1, z);
-    	
-    	
-    	event.getPlayer().getWorld().getBlockAt(loc).setType(Material.CYAN_WOOL);
 	}
 }
