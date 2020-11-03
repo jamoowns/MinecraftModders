d57385e4bd4c3276e998aa8287705c8844446b64
jamoowns <jkkha1@student.monash.edu>
2020-11-04 00:49:36 +1100

    Maybe some zombies

--- src/main/java/me/jamoowns/moddingminecraft/PlayerListener.java
+++ src/main/java/me/jamoowns/moddingminecraft/PlayerListener.java
@@ -13,13 +13,17 @@ import java.util.stream.Collectors;
 import org.bukkit.Bukkit;
 import org.bukkit.Location;
 import org.bukkit.Material;
+import org.bukkit.Sound;
 import org.bukkit.block.Chest;
+import org.bukkit.entity.Bee;
 import org.bukkit.entity.EntityType;
 import org.bukkit.entity.Player;
+import org.bukkit.entity.Zombie;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.player.PlayerBucketFillEvent;
 import org.bukkit.event.player.PlayerEggThrowEvent;
+import org.bukkit.event.player.PlayerInteractEvent;
 import org.bukkit.event.player.PlayerJoinEvent;
 import org.bukkit.inventory.ItemStack;
 import org.bukkit.plugin.java.JavaPlugin;
@@ -50,32 +54,38 @@ public final class PlayerListener implements Listener {
 			
 			@Override
 			public void run() {
-				List<? extends Player> players = Bukkit.getOnlinePlayers().stream().collect(Collectors.toList());
-				
+				List<Player> players = Bukkit.getOnlinePlayers().stream().collect(Collectors.toList());
 				Player p = players.get(RANDOM.nextInt(players.size()));
 				
-				Location chestLocation = p.getLocation().add(RANDOM.nextInt(60)-30, 1, RANDOM.nextInt(60)-30);
-				
-				if (p.getWorld().getBlockAt(chestLocation).isEmpty()) {
-					p.getWorld().getBlockAt(chestLocation).setType(Material.CHEST);
-					
-					 Chest chest = (Chest) p.getLocation().getBlock().getState();
-					 
-					 
-					 List<Material> materials = Arrays.asList(Material.values());
-					 
-					 List<Material> itemsForChest = new ArrayList<>();
-					 
-					 for (int i = 0; i < RANDOM.nextInt(5) + 1; i++) {
-						 itemsForChest.add(materials.get(RANDOM.nextInt(materials.size())));
-					 }
-					 
-					 List<ItemStack> forChest = itemsForChest.stream().map(ItemStack::new).collect(Collectors.toList());
-					 
-					 chest.getInventory().setContents(forChest.toArray(new ItemStack[forChest.size()]));
+				int attempts = 0;
+				boolean done = false;
+				while (attempts < 10 && !done) {
+					Location chestLocation = p.getLocation().add(RANDOM.nextInt(40)-20, 3, RANDOM.nextInt(40)-20);
+					if (p.getWorld().getBlockAt(chestLocation).isEmpty()) {
+						done = true;
+						p.getWorld().playSound(p.getLocation(), Sound.BLOCK_GLASS_BREAK, 10, 1);
+						p.getWorld().playSound(p.getLocation(), Sound.BLOCK_GLASS_BREAK, 15, 1);
+						
+						p.getWorld().getBlockAt(chestLocation).setType(Material.CHEST);
+						
+						 Chest chest = (Chest) p.getLocation().getBlock().getState();
+						 
+						 
+						 List<Material> materials = Arrays.asList(Material.values());
+						 
+						 List<Material> itemsForChest = new ArrayList<>();
+						 
+						 for (int i = 0; i < RANDOM.nextInt(5) + 1; i++) {
+							 itemsForChest.add(materials.get(RANDOM.nextInt(materials.size())));
+						 }
+						 
+						 List<ItemStack> forChest = itemsForChest.stream().map(ItemStack::new).collect(Collectors.toList());
+						 
+						 chest.getInventory().setContents(forChest.toArray(new ItemStack[forChest.size()]));
+					}
 				}
 			}
-		}, RANDOM.nextInt(120000) + 60000, RANDOM.nextInt(120000) + 60000);
+		}, RANDOM.nextInt(100000) + 50000, RANDOM.nextInt(100000) + 50000);
 	}
 	
     @EventHandler
@@ -92,5 +102,16 @@ public final class PlayerListener implements Listener {
     @EventHandler
 	public void onPlayerEggThrowEvent(PlayerEggThrowEvent event) {
     	event.setHatchingType(EntityType.WITCH);
+    }
+    
+    @EventHandler
+	public void onPlayerInteractEvent(PlayerInteractEvent event) {
+    	if (event.getClickedBlock().getType() == Material.BELL) {
+    		Player player = event.getPlayer();
+    		Location dundundun = player.getLocation().add(player.getLocation().getDirection().multiply(-30));
+
+        	Zombie zombie =  player.getWorld().spawn(dundundun, Zombie.class);
+        	zombie.setTarget(player);
+    	}
     }
 }
