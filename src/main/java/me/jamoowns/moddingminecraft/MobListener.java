fef2e4fdd41b6bc3e7584baef5661f37134ff1ad
mabmo <martin.o.94@hotmail.com>
2020-11-18 00:07:45 +1100

    Update MobListener.java

--- src/main/java/me/jamoowns/moddingminecraft/MobListener.java
+++ src/main/java/me/jamoowns/moddingminecraft/MobListener.java
@@ -196,7 +196,6 @@ public class MobListener implements Listener {
                             !blockBehindPlayer.isLiquid() &&
                             !blockBelowBehindPlayer.getType().name().contains("CARPET") &&
                             !blockBelowBehindPlayer.getType().name().contains("SNOW") &&
-                            !blockBelowBehindPlayer.getType().name().contains("GRASS") &&
                             !blockBelowBehindPlayer.isLiquid() &&
                             !blockBelowBehindPlayer.isEmpty()) {
                         List<Block> trail = trailByPlayer.getOrDefault(event.getPlayer().getUniqueId(), new ArrayList<>());
