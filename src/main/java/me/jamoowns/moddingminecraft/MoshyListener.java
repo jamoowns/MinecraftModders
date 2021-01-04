c466b14a08e12fbf633d9081deb9a352f3673614
jamoowns <james.kharsas@gmail.com>
2021-01-04 20:54:28 +1100

    change to updated chat listener

--- src/main/java/me/jamoowns/moddingminecraft/MoshyListener.java
+++ src/main/java/me/jamoowns/moddingminecraft/MoshyListener.java
@@ -3,13 +3,13 @@ package me.jamoowns.moddingminecraft;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
-import org.bukkit.event.player.PlayerChatEvent;
+import org.bukkit.event.player.AsyncPlayerChatEvent;
 import org.bukkit.util.Vector;
 
 public class MoshyListener implements Listener {
 
 	@EventHandler
-	public void onPlayerJoin(PlayerChatEvent event) {
+	public void onAsyncPlayerChatEvent(AsyncPlayerChatEvent event) {
 		Player flyingChat = event.getPlayer();
 		flyingChat.setVelocity(new Vector(0, 10, 0));
 		flyingChat.setGliding(true);
