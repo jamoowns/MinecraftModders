875a7000486638160d0633ea16b9d907bcc278c6
jamoowns <james.kharsas@gmail.com>
2021-01-08 23:58:27 +1100

    messin with direction

--- src/main/java/me/jamoowns/moddingminecraft/customitems/CustomItemListener.java
+++ src/main/java/me/jamoowns/moddingminecraft/customitems/CustomItemListener.java
@@ -48,8 +48,8 @@ public final class CustomItemListener implements Listener {
 		Block target = event.getPlayer().getTargetBlockExact(30);
 		if (target != null) {
 			for (double d = 0; d <= 30; d += 0.1) {
-				target.getWorld().spawnParticle(Particle.TOTEM,
-						event.getPlayer().getLocation().add(event.getPlayer().getLocation().getDirection().multiply(d)),
+				target.getWorld().spawnParticle(Particle.TOTEM, event.getPlayer().getLocation()
+						.add(target.getLocation().subtract(event.getPlayer().getLocation()).getDirection().multiply(d)),
 						1, 0, 0, 0, 0);
 			}
 		}
