0f6b093b13dec20197c7278b73c3643775f336ea
mabmo <martin.o.94@hotmail.com>
2021-01-09 22:58:47 +1100

    Update MabSchematic.java

--- src/main/java/me/jamoowns/moddingminecraft/MabSchematic.java
+++ src/main/java/me/jamoowns/moddingminecraft/MabSchematic.java
@@ -22,31 +22,19 @@ public class MabSchematic implements Listener {
 
 			Material[][][] buildGrid = new Material[10][15][20];
 
-			for (int x = 0; x < buildGrid[0].length; x++) {
-				for (int y = 0; y < buildGrid[1].length; y++) {
-					for (int z = 0; z < buildGrid[2].length; z++) {
-						if (x < 1 && x > buildGrid[0].length) {
-							if (y < 1 && y > buildGrid[1].length) {
-								if (z < 1 && z > buildGrid[2].length) {
-									buildGrid[x][y][z] = buildList[1];
-								}
-							}
-						}
-					}
-				}
-			}
+			/*
+			 * for (int x = 0; x < buildGrid[0].length; x++) { for (int y = 0; y <
+			 * buildGrid[1].length; y++) { for (int z = 0; z < buildGrid[2].length; z++) {
+			 * if (x < 1 && x > buildGrid[0].length) { if (y < 1 && y > buildGrid[1].length)
+			 * { if (z < 1 && z > buildGrid[2].length) { buildGrid[x][y][z] = buildList[1];
+			 * } } } } } }
+			 */
 
-			for (int x = 0; x < buildGrid[0].length; x++) {
-				for (int y = 0; y < buildGrid[1].length; y++) {
-					for (int z = 0; z < buildGrid[2].length; z++) {
-						if (x < 1 && x > buildGrid[0].length) {
-							if (y < 1 && y > buildGrid[1].length) {
-								if (z < 1 && z > buildGrid[2].length) {
-									Location temploc = loc;
-									loc.getWorld().getBlockAt(temploc.add(x, y, z)).setType(buildGrid[x][y][z]);
-								}
-							}
-						}
+			for (int r = 0; r < buildGrid[0].length; r++) {
+				for (int c = 0; c < buildGrid[1].length; c++) {
+					for (int l = 0; l < buildGrid[2].length; l++) {
+						Location temploc = loc;
+						loc.getWorld().getBlockAt(temploc.add(r, l, c)).setType(Material.STONE_BRICKS);
 					}
 				}
 			}
