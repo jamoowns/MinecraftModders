599b781e5ca9028bd761e1f02b369c677ed61300
mabmo <martin.o.94@hotmail.com>
2021-01-09 23:23:55 +1100

    Update MabListener.java

--- src/main/java/me/jamoowns/moddingminecraft/MabListener.java
+++ src/main/java/me/jamoowns/moddingminecraft/MabListener.java
@@ -103,15 +103,13 @@ public class MabListener implements Listener {
 			 * { if (z < 1 && z > buildGrid[2].length) { buildGrid[x][y][z] = buildList[1];
 			 * } } } } } }
 			 */
-			for (int c = 0; c < buildGrid[1].length; c++) {
-				for (int r = 0; r < buildGrid[0].length; r++) {
-					Location temploc = loc.clone();
-					loc.getWorld().getBlockAt(temploc.add(0 + r, 0, 0 + c)).setType(Material.STONE_BRICKS);
-				}
-			}
 			for (int l = 0; l < buildGrid[2].length; l++) {
-				Location temploc = loc.clone();
-				loc.getWorld().getBlockAt(temploc.add(0, 1, 0)).setType(Material.STONE_BRICKS);
+				for (int c = 0; c < buildGrid[1].length; c++) {
+					for (int r = 0; r < buildGrid[0].length; r++) {
+						Location temploc = loc.clone();
+						loc.getWorld().getBlockAt(temploc.add(0 + r, 0 + l, 0 + c)).setType(Material.STONE_BRICKS);
+					}
+				}
 			}
 
 		}
