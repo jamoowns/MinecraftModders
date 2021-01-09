1940ec4b8d3c0b6d2ac8ed419e4c689460e732e7
mabmo <martin.o.94@hotmail.com>
2021-01-10 04:13:28 +1100

    Update MabListener.java

--- src/main/java/me/jamoowns/moddingminecraft/MabListener.java
+++ src/main/java/me/jamoowns/moddingminecraft/MabListener.java
@@ -275,11 +275,11 @@ public class MabListener implements Listener {
 						if (c == 4 && r == 3) {
 							insert(0 + r, 0 + heightTracker, 0 + c, buildList[3], 4, 0, 1);
 						} else if (c == 4 && r == 5) {
-							insert(0 + r, 0 + heightTracker, 0 + c, buildList[3], 4, 0, 2);
+							insert(0 + r, 0 + heightTracker, 0 + c, buildList[6], 0, 0, 0);
 						} else if (c == 6 && r == 3) {
-							insert(0 + r, 0 + heightTracker, 0 + c, buildList[3], 4, 0, 3);
+							insert(0 + r, 0 + heightTracker, 0 + c, buildList[6], 0, 0, 0);
 						} else if (c == 6 && r == 5) {
-							insert(0 + r, 0 + heightTracker, 0 + c, buildList[3], 4, 0, 4);
+							insert(0 + r, 0 + heightTracker, 0 + c, buildList[6], 0, 0, 0);
 						} else {
 							insert(0 + r, 0 + heightTracker, 0 + c, buildList[3], 4, 0, 0);
 						}
