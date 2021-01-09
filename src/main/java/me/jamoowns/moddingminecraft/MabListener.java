b9ccc2bbb4b603a6ee20ce828d00e2288bab1077
mabmo <martin.o.94@hotmail.com>
2021-01-10 08:46:24 +1100

    Update MabListener.java

--- src/main/java/me/jamoowns/moddingminecraft/MabListener.java
+++ src/main/java/me/jamoowns/moddingminecraft/MabListener.java
@@ -172,6 +172,38 @@ public class MabListener implements Listener {
 
 					}
 				}
+
+				heightTracker++;
+				for (int c = 0; c < buildGrid[0][0].length; c++) {
+					for (int r = 0; r < buildGrid.length; r++) {
+						if (r == 15 || r == 1) {
+							if (c % 2 == 1) {
+								insert(0 + r, 0 + heightTracker, 0 + c, buildList[1], 0, 0, 0);
+							}
+						} else if (r == 0) {
+							if (c % 2 == 0) {
+								insert(0 + r, 0 + heightTracker, 0 + c, buildList[3], 1, 1, 0);
+							} else {
+								insert(0 + r, 0 + heightTracker, 0 + c, buildList[3], 3, 0, 0);
+							}
+						} else if (r == 16) {
+							if (c % 2 == 0) {
+								insert(0 + r, 0 + heightTracker, 0 + c, buildList[3], 3, 1, 0);
+							} else {
+								insert(0 + r, 0 + heightTracker, 0 + c, buildList[3], 1, 0, 0);
+							}
+						} else if ((r == 5 || r == 11)) {
+							if (c < 3 || c > 6) {
+								insert(0 + r, 0 + heightTracker, 0 + c, buildList[1], 0, 0, 0);
+							} else if (c == 3) {
+								insert(0 + r, 0 + heightTracker, 0 + c, buildList[3], 2, 0, 0);
+							} else if (c == 6) {
+								insert(0 + r, 0 + heightTracker, 0 + c, buildList[3], 4, 0, 0);
+							}
+						}
+					}
+				}
+
 			} else {
 				for (; heightTracker < 19; heightTracker++) {
 					for (int c = 0; c < buildGrid[0][0].length; c++) {
