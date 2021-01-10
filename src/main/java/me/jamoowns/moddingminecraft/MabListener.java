f6964e0bea0103330a828b67e1d07a82fbb17cd8
mabmo <martin.o.94@hotmail.com>
2021-01-10 23:22:17 +1100

    Update MabListener.java

--- src/main/java/me/jamoowns/moddingminecraft/MabListener.java
+++ src/main/java/me/jamoowns/moddingminecraft/MabListener.java
@@ -601,30 +601,21 @@ public class MabListener implements Listener {
 			}
 		}
 		if (stage == 2) {
-			if ((cCount == 0 && rCount < 1) || (rCount == 0 && cCount < 1) || (cCount == 1 && rCount < 2)
-					|| (rCount == 1 && cCount < 2) || (cCount == 5 && rCount < 6) || (rCount == 5 && cCount < 6)
-					|| (cCount == 11 && rCount < 12) || (rCount == 11 && cCount < 12)) {
-				insert(0 + r, 0 + heightTracker, 0 + c, buildList[1], 0, 0, 0);
-			} else if (cCount == 15 && rCount < 15) {
+			if (cCount == 15 && rCount < 15 && rCount > 1) {
 				if (rCount % 2 == 0) {
 					insert(0 + r, 0 + heightTracker, 0 + c, buildList[1], 0, 0, 0);
 				} else {
 					insert(0 + r, 0 + heightTracker, 0 + c, buildList[3], 2, 0, 0);
 				}
-			} else if (rCount == 15 && cCount < 15) {
+			} else if (rCount == 15 && cCount < 15 && cCount > 1) {
 				if (cCount % 2 == 0) {
 					insert(0 + r, 0 + heightTracker, 0 + c, buildList[1], 0, 0, 0);
 				} else {
 					insert(0 + r, 0 + heightTracker, 0 + c, buildList[3], 1, 0, 0);
 				}
-			} else if (rCount == 15 && cCount == 15) {
+			} else if ((rCount == 15 && cCount == 15) || (rCount == 1 && cCount == 15) || (rCount == 15 && cCount == 1)
+					|| (rCount == 1 && cCount == 1)) {
 				insert(0 + r, 0 + heightTracker, 0 + c, buildList[1], 0, 0, 0);
-			} else if ((cCount == 8 && rCount < 9) || (rCount == 8 && cCount < 9) || (cCount == 9 && rCount < 10)
-					|| (rCount == 9 && cCount < 10) || (cCount == 7 && rCount < 8) || (rCount == 7 && cCount < 8)) {
-				insert(0 + r, 0 + heightTracker, 0 + c, buildList[2], 0, 0, 0);
-			} else if ((cCount == 2 && rCount < 3) || (rCount == 2 && cCount < 3) || (cCount == 3 && rCount < 4)
-					|| (rCount == 3 && cCount < 4) || (cCount == 4 && rCount < 5) || (rCount == 4 && cCount < 5)) {
-				insert(0 + r, 0 + heightTracker, 0 + c, buildList[2], 0, 0, 0);
 			} else if ((cCount == 14 && rCount < 15) || (rCount == 14 && cCount < 15) || (cCount == 12 && rCount < 13)
 					|| (rCount == 12 && cCount < 13) || (cCount == 13 && rCount < 14)
 					|| (rCount == 13 && cCount < 14)) {
