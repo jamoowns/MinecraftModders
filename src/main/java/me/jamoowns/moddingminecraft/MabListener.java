f6ce8b4eacf01e741e4c349b2a28f9f0820dbf24
mabmo <martin.o.94@hotmail.com>
2021-01-10 22:37:57 +1100

    Update MabListener.java

--- src/main/java/me/jamoowns/moddingminecraft/MabListener.java
+++ src/main/java/me/jamoowns/moddingminecraft/MabListener.java
@@ -154,34 +154,32 @@ public class MabListener implements Listener {
 
 			heightTracker++;
 			cCount = 0;
-			for (; heightTracker < 19; heightTracker++) {
+			if (direction == BlockFace.NORTH || direction == BlockFace.WEST) {
+				cCount = buildGrid[0][0].length;
+			} else if (direction == BlockFace.SOUTH || direction == BlockFace.EAST) {
+				cCount = -1;
+			}
+			int rCount = 0;
+			for (int c = 0; c < buildGrid[0][0].length; c++) {
+
 				if (direction == BlockFace.NORTH || direction == BlockFace.WEST) {
-					cCount = buildGrid[0][0].length;
+					cCount--;
 				} else if (direction == BlockFace.SOUTH || direction == BlockFace.EAST) {
-					cCount = -1;
+					cCount++;
 				}
-				int rCount = 0;
-				for (int c = 0; c < buildGrid[0][0].length; c++) {
 
-					if (direction == BlockFace.NORTH || direction == BlockFace.WEST) {
-						cCount--;
-					} else if (direction == BlockFace.SOUTH || direction == BlockFace.EAST) {
-						cCount++;
-					}
-
+				if (direction == BlockFace.NORTH || direction == BlockFace.SOUTH) {
+					rCount = buildGrid.length;
+				} else if (direction == BlockFace.WEST || direction == BlockFace.EAST) {
+					rCount = -1;
+				}
+				for (int r = 0; r < buildGrid.length; r++) {
 					if (direction == BlockFace.NORTH || direction == BlockFace.SOUTH) {
-						rCount = buildGrid.length;
+						rCount--;
 					} else if (direction == BlockFace.WEST || direction == BlockFace.EAST) {
-						rCount = -1;
+						rCount++;
 					}
-					for (int r = 0; r < buildGrid.length; r++) {
-						if (direction == BlockFace.NORTH || direction == BlockFace.SOUTH) {
-							rCount--;
-						} else if (direction == BlockFace.WEST || direction == BlockFace.EAST) {
-							rCount++;
-						}
-						CornerGrid(r, c, rCount, cCount, heightTracker, 2, buildList);
-					}
+					CornerGrid(r, c, rCount, cCount, heightTracker, 2, buildList);
 				}
 			}
 
@@ -604,7 +602,6 @@ public class MabListener implements Listener {
 			}
 		}
 		if (stage == 2) {
-			heightTracker++;
 			if ((cCount == 1 && rCount < 2) || (rCount == 1 && cCount < 2) || (cCount == 4 && rCount < 5)
 					|| (rCount == 4 && cCount < 5)) {
 				insert(0 + r, 0 + heightTracker, 0 + c, buildList[2], 0, 0, 0);
