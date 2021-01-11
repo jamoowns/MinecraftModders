331e52771267d926dade4308c885db780d5ca626
mabmo <martin.o.94@hotmail.com>
2021-01-11 21:46:41 +1100

    Update MabListener.java

--- src/main/java/me/jamoowns/moddingminecraft/MabListener.java
+++ src/main/java/me/jamoowns/moddingminecraft/MabListener.java
@@ -593,11 +593,14 @@ public class MabListener implements Listener {
 			int openings, BlockFace leftRight) {
 		if (stage == 1) {
 
-			if (((cCount == 15 || cCount == 1) && rCount < 15 && rCount > 1)
-					|| ((rCount == 15 || rCount == 1) && cCount < 15 && cCount > 1)) {
+			if (((cCount == 15 || cCount == 1) && rCount < 16 && rCount > 0)
+					|| ((rCount == 15 || rCount == 1) && cCount < 16 && cCount > 0)) {
 				insert(0 + r, 0 + heightTracker, 0 + c, buildList[1], 0, 0, 0);
 			}
 			if ((cCount == 1 && rCount < 2) || (rCount == 1 && cCount < 2)) {
+				insert(0 + r, 0 + heightTracker, 0 + c, buildList[1], 0, 0, 0);
+			}
+			if ((cCount == 15 && rCount < 2) || (rCount == 15 && cCount < 2)) {
 				insert(0 + r, 0 + heightTracker, 0 + c, buildList[1], 0, 0, 0);
 			}
 
