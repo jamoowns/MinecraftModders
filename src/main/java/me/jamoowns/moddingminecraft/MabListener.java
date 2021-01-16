10ea1c80200b2e6f5f07b2a18833c1884f27c7a3
mabmo <martin.o.94@hotmail.com>
2021-01-16 19:19:49 +1100

    Update MabListener.java

--- src/main/java/me/jamoowns/moddingminecraft/MabListener.java
+++ src/main/java/me/jamoowns/moddingminecraft/MabListener.java
@@ -741,8 +741,9 @@ public class MabListener implements Listener {
 			if (rCount == 0 && cCount < 16 && cCount > 0) {
 				if (openings != 5 && openings != 2 && openings != 3 && openings != 4) {
 					if (cCount % 2 == 1) {
+						insert(0 + r, 0 + heightTracker, 0 + c, buildList[3], getStairFace(3, leftRight), 0, 0);
 					} else {
-						insert(0 + r, 0 + heightTracker, 0 + c, buildList[3], getStairFace(3, leftRight), 1, 0);
+						insert(0 + r, 0 + heightTracker, 0 + c, buildList[3], getStairFace(1, leftRight), 1, 0);
 					}
 				}
 			} else if (rCount == 1 && cCount < 15 && cCount > 1) {
@@ -764,6 +765,7 @@ public class MabListener implements Listener {
 			} else if (rCount == 16 && cCount < 16 && cCount > 0) {
 				if (openings != 4) {
 					if (cCount % 2 == 1) {
+						insert(0 + r, 0 + heightTracker, 0 + c, buildList[3], getStairFace(3, leftRight), 0, 0);
 					} else {
 						insert(0 + r, 0 + heightTracker, 0 + c, buildList[3], getStairFace(1, leftRight), 1, 0);
 					}
@@ -771,6 +773,7 @@ public class MabListener implements Listener {
 			} else if (cCount == 0 && rCount < 16 && rCount > 0) {
 				if (openings != 2 && openings != 3 && openings != 4) {
 					if (rCount % 2 == 1) {
+						insert(0 + r, 0 + heightTracker, 0 + c, buildList[3], getStairFace(2, leftRight), 0, 0);
 					} else {
 						insert(0 + r, 0 + heightTracker, 0 + c, buildList[3], getStairFace(4, leftRight), 1, 0);
 					}
@@ -794,6 +797,7 @@ public class MabListener implements Listener {
 			} else if (cCount == 16 && rCount < 16 && rCount > 0) {
 				if (openings != 3 && openings != 4) {
 					if (rCount % 2 == 1) {
+						insert(0 + r, 0 + heightTracker, 0 + c, buildList[3], getStairFace(4, leftRight), 0, 0);
 					} else {
 						insert(0 + r, 0 + heightTracker, 0 + c, buildList[3], getStairFace(2, leftRight), 1, 0);
 					}
