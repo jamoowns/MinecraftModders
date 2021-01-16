35499331bb3d31a0de88411705e15bc07f6c2e0b
mabmo <martin.o.94@hotmail.com>
2021-01-16 23:34:04 +1100

    Update MabListener.java

--- src/main/java/me/jamoowns/moddingminecraft/MabListener.java
+++ src/main/java/me/jamoowns/moddingminecraft/MabListener.java
@@ -1054,7 +1054,12 @@ public class MabListener implements Listener {
 						insert(0 + r, 0 + heightTracker, 0 + c, buildList[3], getStairFace(3, leftRight), 0, 0);
 					}
 				} else {
-					insert(0 + r, 0 + heightTracker, 0 + c, buildList[2], 2, 0, 0);
+					if (cCount == 5 || cCount == 11) {
+						insert(0 + r, 0 + heightTracker, 0 + c, buildList[1], 0, 0, 0);
+					} else {
+						insert(0 + r, 0 + heightTracker, 0 + c, buildList[2], 2, 0, 0);
+
+					}
 				}
 			} else if (rCount == 1 && cCount < 15 && cCount > 1) {
 				if (openings != 5 && openings != 2 && openings != 3 && openings != 4) {
@@ -1086,7 +1091,12 @@ public class MabListener implements Listener {
 						insert(0 + r, 0 + heightTracker, 0 + c, buildList[3], getStairFace(1, leftRight), 0, 0);
 					}
 				} else {
-					insert(0 + r, 0 + heightTracker, 0 + c, buildList[2], 2, 0, 0);
+					if (cCount == 5 || cCount == 11) {
+						insert(0 + r, 0 + heightTracker, 0 + c, buildList[1], 0, 0, 0);
+					} else {
+						insert(0 + r, 0 + heightTracker, 0 + c, buildList[2], 2, 0, 0);
+
+					}
 				}
 			} else if (cCount == 0 && rCount < 16 && rCount > 0) {
 				if (openings != 2 && openings != 3 && openings != 4) {
@@ -1096,7 +1106,12 @@ public class MabListener implements Listener {
 						insert(0 + r, 0 + heightTracker, 0 + c, buildList[3], getStairFace(4, leftRight), 0, 0);
 					}
 				} else {
-					insert(0 + r, 0 + heightTracker, 0 + c, buildList[2], 2, 0, 0);
+					if (cCount == 5 || cCount == 11) {
+						insert(0 + r, 0 + heightTracker, 0 + c, buildList[1], 0, 0, 0);
+					} else {
+						insert(0 + r, 0 + heightTracker, 0 + c, buildList[2], 2, 0, 0);
+
+					}
 				}
 			} else if (cCount == 1 && rCount < 15 && rCount > 1) {
 				if (openings != 2 && openings != 3 && openings != 4) {
@@ -1128,7 +1143,12 @@ public class MabListener implements Listener {
 						insert(0 + r, 0 + heightTracker, 0 + c, buildList[3], getStairFace(2, leftRight), 0, 0);
 					}
 				} else {
-					insert(0 + r, 0 + heightTracker, 0 + c, buildList[2], 2, 0, 0);
+					if (cCount == 5 || cCount == 11) {
+						insert(0 + r, 0 + heightTracker, 0 + c, buildList[1], 0, 0, 0);
+					} else {
+						insert(0 + r, 0 + heightTracker, 0 + c, buildList[2], 2, 0, 0);
+
+					}
 				}
 			} else if ((rCount == 15 && cCount == 15) || (rCount == 1 && cCount == 15) || (rCount == 15 && cCount == 1)
 					|| (rCount == 1 && cCount == 1)) {
