b3efaa637ef987d42f7c8708b8c9b1322c48a9f2
mabmo <martin.o.94@hotmail.com>
2021-01-16 17:47:42 +1100

    Update MabListener.java

--- src/main/java/me/jamoowns/moddingminecraft/MabListener.java
+++ src/main/java/me/jamoowns/moddingminecraft/MabListener.java
@@ -626,7 +626,7 @@ public class MabListener implements Listener {
 		}
 		if (stage == 2) {
 			if (rCount == 0 && cCount < 16 && cCount > 0) {
-				if (openings != 5 && openings != 2 && openings != 3 && openings != 4) {
+				if ((openings != 5 || oneWay == 2) && openings != 2 && openings != 3 && openings != 4) {
 					if (rCount % 2 == 0) {
 						insert(0 + r, 0 + heightTracker, 0 + c, buildList[1], 0, 0, 0);
 					} else {
@@ -646,7 +646,7 @@ public class MabListener implements Listener {
 					insert(0 + r, 0 + heightTracker, 0 + c, buildList[2], 2, 0, 0);
 				}
 			} else if (cCount == 0 && rCount < 16 && rCount > 0) {
-				if (openings != 2 && openings != 3 && openings != 4) {
+				if ((openings != 5 || oneWay == 1) && openings != 2 && openings != 3 && openings != 4) {
 					if (rCount % 2 == 0) {
 						insert(0 + r, 0 + heightTracker, 0 + c, buildList[1], 0, 0, 0);
 					} else {
@@ -676,7 +676,7 @@ public class MabListener implements Listener {
 					insert(0 + r, 0 + heightTracker, 0 + c, buildList[2], 2, 0, 0);
 				}
 			} else if (cCount == 1 && rCount < 15 && rCount > 1) {
-				if (openings != 2 && openings != 3 && openings != 4) {
+				if ((openings != 5 || oneWay == 1) && openings != 2 && openings != 3 && openings != 4) {
 					if (rCount % 2 == 0) {
 						insert(0 + r, 0 + heightTracker, 0 + c, buildList[1], 0, 0, 0);
 					} else {
@@ -696,7 +696,7 @@ public class MabListener implements Listener {
 					insert(0 + r, 0 + heightTracker, 0 + c, buildList[2], 2, 0, 0);
 				}
 			} else if (rCount == 1 && cCount < 15 && cCount > 1) {
-				if (openings != 5 && openings != 2 && openings != 3 && openings != 4) {
+				if ((openings != 5 || oneWay == 2) && openings != 2 && openings != 3 && openings != 4) {
 					if (cCount % 2 == 0) {
 						insert(0 + r, 0 + heightTracker, 0 + c, buildList[1], 0, 0, 0);
 					} else {
