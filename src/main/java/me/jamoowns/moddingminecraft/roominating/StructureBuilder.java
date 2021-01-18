373b36292c8dbcf0bd77d48684267f6eb1cc2630
mabmo <martin.o.94@hotmail.com>
2021-01-18 22:50:30 +1100

    Update StructureBuilder.java

--- src/main/java/me/jamoowns/moddingminecraft/roominating/StructureBuilder.java
+++ src/main/java/me/jamoowns/moddingminecraft/roominating/StructureBuilder.java
@@ -1284,28 +1284,28 @@ public final class StructureBuilder {
 			} else if (cCount == 5 || cCount == 10) {
 				insert(0 + r, 0 + heightTracker, 0 + c, buildList[1], 0, 0, 0);
 			} else if (cCount == 6) {
-				insert(0 + r, 0 + heightTracker, 0 + c, buildList[3], 2, 0, 0);
+				insert(0 + r, 0 + heightTracker, 0 + c, buildList[3], getStairFace(2, leftRight), 0, 0);
 			} else if (cCount == 9) {
-				insert(0 + r, 0 + heightTracker, 0 + c, buildList[3], 4, 0, 0);
+				insert(0 + r, 0 + heightTracker, 0 + c, buildList[3], getStairFace(4, leftRight), 0, 0);
 			} else if (cCount == 1) {
 				if (rCount % 2 == 0) {
-					insert(0 + r, 0 + heightTracker, 0 + c, buildList[3], 4, 0, 0);
+					insert(0 + r, 0 + heightTracker, 0 + c, buildList[3], getStairFace(4, leftRight), 0, 0);
 				} else {
 					insert(0 + r, 0 + heightTracker, 0 + c, buildList[1], 0, 0, 0);
 				}
 			} else if (cCount == 14) {
 				if (rCount % 2 == 0) {
-					insert(0 + r, 0 + heightTracker, 0 + c, buildList[3], 2, 0, 0);
+					insert(0 + r, 0 + heightTracker, 0 + c, buildList[3], getStairFace(2, leftRight), 0, 0);
 				} else {
 					insert(0 + r, 0 + heightTracker, 0 + c, buildList[1], 0, 0, 0);
 				}
 			} else if (cCount == 0) {
 				if (rCount % 2 == 1) {
-					insert(0 + r, 0 + heightTracker, 0 + c, buildList[3], 4, 1, 0);
+					insert(0 + r, 0 + heightTracker, 0 + c, buildList[3], getStairFace(4, leftRight), 1, 0);
 				}
 			} else if (cCount == 15) {
 				if (rCount % 2 == 1) {
-					insert(0 + r, 0 + heightTracker, 0 + c, buildList[3], 2, 1, 0);
+					insert(0 + r, 0 + heightTracker, 0 + c, buildList[3], getStairFace(2, leftRight), 1, 0);
 				}
 			}
 		} else if (stage == 3 || stage == 7) {
