bf53381039cce797b334734892e27a6e7abde3bf
mabmo <martin.o.94@hotmail.com>
2021-01-18 20:02:45 +1100

    Update MabListener.java

--- src/main/java/me/jamoowns/moddingminecraft/MabListener.java
+++ src/main/java/me/jamoowns/moddingminecraft/MabListener.java
@@ -160,8 +160,9 @@ public class MabListener implements IGameEventListener {
 			Block block = event.getBlockPlaced();
 			Material mat = block.getType();
 			block.setType(Material.AIR);
-			block.getLocation().add(.5, .5, .5);
-			block.getLocation().getWorld().spawnFallingBlock(block.getLocation(), mat, (byte) 0);
+			Location loc = block.getLocation();
+			loc.add(.5, .5, .5);
+			block.getLocation().getWorld().spawnFallingBlock(loc, mat, (byte) 0);
 
 		}
 		if (event.getBlock().getType().equals(Material.BASALT)) {
