57436f980ebee9894d850f4366da659adf35f076
mabmo <martin.o.94@hotmail.com>
2021-01-19 23:56:44 +1100

    Update MabListener.java

--- src/main/java/me/jamoowns/moddingminecraft/MabListener.java
+++ src/main/java/me/jamoowns/moddingminecraft/MabListener.java
@@ -170,11 +170,11 @@ public class MabListener implements IGameEventListener {
 			if (javaPlugin.featureTracker().isFeatureActive(Feature.LIGHT_BLOCKS)) {
 				fb.setVelocity(new Vector(0, 1, 0));
 				Random r = new Random();
-				int low = -10;
-				int high = 10;
-				float x = r.nextInt(high - low) + low / 10;
+				int low = -50;
+				int high = 50;
+				float x = r.nextInt(high - low) + low / 100;
 
-				float y = r.nextInt(high - low) + low / 10;
+				float y = r.nextInt(high - low) + low / 100;
 				fb.setVelocity(new Vector(x, 2, y));
 			}
 		}
