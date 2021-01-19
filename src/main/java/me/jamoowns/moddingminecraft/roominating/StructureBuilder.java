e298abf622b32dbd6029181d0ba0d4abd3339c5a
mabmo <martin.o.94@hotmail.com>
2021-01-19 20:44:37 +1100

    Update StructureBuilder.java

--- src/main/java/me/jamoowns/moddingminecraft/roominating/StructureBuilder.java
+++ src/main/java/me/jamoowns/moddingminecraft/roominating/StructureBuilder.java
@@ -1442,22 +1442,39 @@ public final class StructureBuilder {
 			} else if (cCount == 3 || cCount == 7 || cCount == 8 || cCount == 12) {
 				insert(0 + r, 0 + heightTracker, 0 + c, buildList[5], 0, 1, 0);
 			} else if (cCount == 4 || cCount == 9 || cCount == 13) {
-				insert(0 + r, 0 + heightTracker, 0 + c, buildList[3], 4, 1, 0);
+				insert(0 + r, 0 + heightTracker, 0 + c, buildList[3], getStairFace(4, leftRight), 1, 0);
 			} else if (cCount == 2 || cCount == 6 || cCount == 11) {
-				insert(0 + r, 0 + heightTracker, 0 + c, buildList[3], 2, 1, 0);
+				insert(0 + r, 0 + heightTracker, 0 + c, buildList[3], getStairFace(2, leftRight), 1, 0);
 			} else if (cCount == 3 || cCount == 6 || cCount == 11) {
-				insert(0 + r, 0 + heightTracker, 0 + c, buildList[3], 2, 1, 0);
+				insert(0 + r, 0 + heightTracker, 0 + c, buildList[3], getStairFace(2, leftRight), 1, 0);
 			} else if (cCount == 0) {
-				if (rCount % 2 == 0) {
-					insert(0 + r, 0 + heightTracker, 0 + c, buildList[3], 4, 1, 0);
+				if (rCount < chunkSize / 2) {
+					if (rCount % 2 == 0) {
+						insert(0 + r, 0 + heightTracker, 0 + c, buildList[3], getStairFace(4, leftRight), 1, 0);
+					} else {
+						insert(0 + r, 0 + heightTracker, 0 + c, buildList[3], getStairFace(4, leftRight), 0, 0);
+					}
 				} else {
-					insert(0 + r, 0 + heightTracker, 0 + c, buildList[3], 4, 0, 0);
+					if (rCount % 2 == 1) {
+						insert(0 + r, 0 + heightTracker, 0 + c, buildList[3], getStairFace(4, leftRight), 1, 0);
+					} else {
+						insert(0 + r, 0 + heightTracker, 0 + c, buildList[3], getStairFace(4, leftRight), 0, 0);
+					}
 				}
+
 			} else if (cCount == 15) {
-				if (rCount % 2 == 0) {
-					insert(0 + r, 0 + heightTracker, 0 + c, buildList[3], 2, 0, 0);
+				if (rCount < chunkSize / 2) {
+					if (rCount % 2 == 0) {
+						insert(0 + r, 0 + heightTracker, 0 + c, buildList[3], getStairFace(2, leftRight), 1, 0);
+					} else {
+						insert(0 + r, 0 + heightTracker, 0 + c, buildList[3], getStairFace(2, leftRight), 0, 0);
+					}
 				} else {
-					insert(0 + r, 0 + heightTracker, 0 + c, buildList[3], 2, 1, 0);
+					if (rCount % 2 == 1) {
+						insert(0 + r, 0 + heightTracker, 0 + c, buildList[3], getStairFace(2, leftRight), 1, 0);
+					} else {
+						insert(0 + r, 0 + heightTracker, 0 + c, buildList[3], getStairFace(2, leftRight), 0, 0);
+					}
 				}
 			}
 		}
