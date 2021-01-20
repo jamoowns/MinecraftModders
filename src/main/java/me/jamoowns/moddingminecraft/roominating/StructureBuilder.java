f81a141362c7dbaae7ae4e4dd1b4590c891e9dce
mabmo <martin.o.94@hotmail.com>
2021-01-20 17:58:32 +1100

    Update StructureBuilder.java

--- src/main/java/me/jamoowns/moddingminecraft/roominating/StructureBuilder.java
+++ src/main/java/me/jamoowns/moddingminecraft/roominating/StructureBuilder.java
@@ -1267,46 +1267,6 @@ public final class StructureBuilder {
 					insert(0 + r, 0 + heightTracker, 0 + c, buildList[1], 0, 0, 0);
 				}
 			}
-		} else if (stage == 9) {
-
-			if (cCount == 0 || cCount == 15) {
-				insert(0 + r, 0 + heightTracker, 0 + c, buildList[1], 0, 0, 0);
-			} else if ((cCount == 5 || cCount == 10)) {
-				if (rCount < 2 || rCount > chunkSize - 3 || (rCount > 6 && rCount < chunkSize - 7)) {
-					insert(0 + r, 0 + heightTracker, 0 + c, buildList[1], 0, 0, 0);
-				} else if (rCount == 2 || rCount == chunkSize - 7) {
-					insert(0 + r, 0 + heightTracker, 0 + c, buildList[3], getStairFace(1, leftRight), 1, 0);
-				} else if (rCount == 6 || rCount == chunkSize - 3) {
-					insert(0 + r, 0 + heightTracker, 0 + c, buildList[3], getStairFace(3, leftRight), 1, 0);
-				}
-			}
-		} else if (stage == 10) {
-
-			if (cCount == 0 || cCount == 10) {
-				insert(0 + r, 0 + heightTracker, 0 + c, buildList[3], getStairFace(4, leftRight), 0, 0);
-			} else if (cCount == 5 || cCount == 15) {
-				insert(0 + r, 0 + heightTracker, 0 + c, buildList[3], getStairFace(2, leftRight), 0, 0);
-			} else if (cCount == 1 || cCount == 11) {
-				insert(0 + r, 0 + heightTracker, 0 + c, buildList[3], getStairFace(2, leftRight), 1, 0);
-			} else if (cCount == 4 || cCount == 14) {
-				insert(0 + r, 0 + heightTracker, 0 + c, buildList[3], getStairFace(4, leftRight), 1, 0);
-			}
-		} else if (stage == 11) {
-
-			if (cCount == 1 || cCount == 12) {
-				insert(0 + r, 0 + heightTracker, 0 + c, buildList[3], getStairFace(4, leftRight), 0, 0);
-			} else if (cCount == 2 || cCount == 13) {
-				insert(0 + r, 0 + heightTracker, 0 + c, buildList[1], 0, 0, 0);
-			} else if (cCount == 3 || cCount == 14) {
-				insert(0 + r, 0 + heightTracker, 0 + c, buildList[3], getStairFace(2, leftRight), 0, 0);
-			} else if (cCount == 4 || cCount == 11) {
-				insert(0 + r, 0 + heightTracker, 0 + c, buildList[5], 0, 0, 0);
-			}
-		} else if (stage == 12) {
-
-			if (cCount == 2 || cCount == 13) {
-				insert(0 + r, 0 + heightTracker, 0 + c, buildList[5], 0, 0, 0);
-			}
 		}
 	}
 
