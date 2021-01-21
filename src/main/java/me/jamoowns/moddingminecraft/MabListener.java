a3f26f710bf57b815ec8b9eb4ee1115e77413059
mabmo <martin.o.94@hotmail.com>
2021-01-21 18:56:05 +1100

    Update MabListener.java

--- src/main/java/me/jamoowns/moddingminecraft/MabListener.java
+++ src/main/java/me/jamoowns/moddingminecraft/MabListener.java
@@ -36,6 +36,7 @@ import org.bukkit.entity.TNTPrimed;
 import org.bukkit.entity.Villager;
 import org.bukkit.entity.Witch;
 import org.bukkit.entity.Zombie;
+import org.bukkit.event.Cancellable;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.EventPriority;
 import org.bukkit.event.block.BlockPlaceEvent;
@@ -48,6 +49,7 @@ import org.bukkit.event.player.PlayerMoveEvent;
 import org.bukkit.event.player.PlayerPortalEvent;
 import org.bukkit.event.weather.ThunderChangeEvent;
 import org.bukkit.event.weather.WeatherChangeEvent;
+import org.bukkit.event.world.ChunkUnloadEvent;
 import org.bukkit.inventory.ItemStack;
 import org.bukkit.inventory.meta.ItemMeta;
 import org.bukkit.inventory.meta.PotionMeta;
@@ -263,6 +265,17 @@ public class MabListener implements IGameEventListener {
 				}
 			}
 		}
+	}
+
+	@EventHandler(priority = EventPriority.HIGHEST)
+	public void onChunkUnloadE(ChunkUnloadEvent event) {
+		if (InputSet && event.getChunk() == Input.getChunk()) {
+			((Cancellable) event).setCancelled(true);
+		}
+		if (OutputSet && event.getChunk() == Output.getChunk()) {
+			((Cancellable) event).setCancelled(true);
+		}
+
 	}
 
 	@EventHandler
@@ -855,12 +868,7 @@ public class MabListener implements IGameEventListener {
 		portalInputItem.setBlockPlaceEvent(event -> {
 
 			Block bl = event.getBlockPlaced();
-			InputSet = true;
-			if (Input != null) {
-				Input.getChunk().setForceLoaded(false);
-			}
 			Input = bl.getLocation();
-			Input.getChunk().setForceLoaded(true);
 			Location loc = bl.getLocation();
 			bl.setType(Material.END_GATEWAY);
 			loc.add(0, 1, 0);
@@ -911,11 +919,7 @@ public class MabListener implements IGameEventListener {
 
 			Block bl = event.getBlockPlaced();
 			OutputSet = true;
-			if (Output != null) {
-				Output.getChunk().setForceLoaded(false);
-			}
 			Output = bl.getLocation();
-			Output.getChunk().setForceLoaded(false);
 			Location loc = bl.getLocation();
 			bl.setType(Material.END_GATEWAY);
 			loc.add(0, 1, 0);
