bb982a01a49bbbb112e078c60f8ebca81e0bdd23
mabmo <martin.o.94@hotmail.com>
2021-01-21 20:29:50 +1100

    Update MabListener.java

--- src/main/java/me/jamoowns/moddingminecraft/MabListener.java
+++ src/main/java/me/jamoowns/moddingminecraft/MabListener.java
@@ -869,14 +869,8 @@ public class MabListener implements IGameEventListener {
 				loc.getBlock().setBlockData(eG.getBlockData());
 				event.getPlayer().getServer().broadcastMessage("Age-" + eG.getAge());
 			}
-			if (loc.getBlock().getState() instanceof EndGateway) {
-				EndGateway eG = (EndGateway) loc.getBlock().getState();
-
-				event.getPlayer().getServer().broadcastMessage("Age-" + eG.getAge());
-				eG.setAge(Long.MAX_VALUE);
-				loc.getBlock().setBlockData(eG.getBlockData());
-				event.getPlayer().getServer().broadcastMessage("Age-" + eG.getAge());
-			}
+			event.getPlayer().getServer()
+					.broadcastMessage("ssssAge-" + ((EndGateway) loc.getBlock().getState()).getAge());
 			loc.add(0, 1, 0);
 			loc.getBlock().setType(Material.END_GATEWAY);
 			if (loc.getBlock().getState() instanceof EndGateway) {
