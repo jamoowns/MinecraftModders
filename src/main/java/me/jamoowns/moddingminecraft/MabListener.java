31bef123fef10cefe53d89b8987d454179b9347a
mabmo <martin.o.94@hotmail.com>
2021-01-21 21:48:06 +1100

    Update MabListener.java

--- src/main/java/me/jamoowns/moddingminecraft/MabListener.java
+++ src/main/java/me/jamoowns/moddingminecraft/MabListener.java
@@ -588,14 +588,16 @@ public class MabListener implements IGameEventListener {
 					Location loc = new Location(Output.getWorld(), (int) Output.getX(), (int) Output.getY(),
 							(int) Output.getZ(), event.getPlayer().getLocation().getYaw(),
 							event.getPlayer().getLocation().getPitch());
-					if (loc.add(0, 0, 1).getBlock().getType().equals(Material.END_GATEWAY)) {
+					if (loc.add(0, 0, 1).getBlock().getType().equals(Material.END_GATEWAY)
+							&& loc.clone().add(0, 0, 1).getBlock().getType().equals(Material.AIR)) {
 
 						if (loc.getYaw() > 90 && loc.getYaw() < 270 || loc.getYaw() < -90 && loc.getYaw() > -270) {
 							loc.setYaw(loc.getYaw() + 180);
 						}
 
 						event.getPlayer().teleport(loc);
-					} else if (loc.add(0, 0, -2).getBlock().getType().equals(Material.END_GATEWAY)) {
+					} else if (loc.add(0, 0, -3).getBlock().getType().equals(Material.END_GATEWAY)
+							&& loc.clone().add(0, 0, -1).getBlock().getType().equals(Material.AIR)) {
 						if (loc.getYaw() < 90 && loc.getYaw() > 270 || loc.getYaw() > -90 && loc.getYaw() < -270) {
 							loc.setYaw(loc.getYaw() + 180);
 						}
@@ -620,12 +622,14 @@ public class MabListener implements IGameEventListener {
 					Location loc = new Location(Input.getWorld(), (int) Input.getX(), (int) Input.getY(),
 							(int) Input.getZ(), event.getPlayer().getLocation().getYaw(),
 							event.getPlayer().getLocation().getPitch());
-					if (loc.add(0, 0, 1).getBlock().getType().equals(Material.END_GATEWAY)) {
+					if (loc.add(0, 0, 1).getBlock().getType().equals(Material.END_GATEWAY)
+							&& loc.clone().add(0, 0, 1).getBlock().getType().equals(Material.AIR)) {
 						if (loc.getYaw() > 90 && loc.getYaw() < 270 || loc.getYaw() < -90 && loc.getYaw() > -270) {
 							loc.setYaw(loc.getYaw() + 180);
 						}
 						event.getPlayer().teleport(loc);
-					} else if (loc.add(0, 0, -2).getBlock().getType().equals(Material.END_GATEWAY)) {
+					} else if (loc.add(0, 0, -2).getBlock().getType().equals(Material.END_GATEWAY)
+							&& loc.clone().add(0, 0, -1).getBlock().getType().equals(Material.AIR)) {
 						if (loc.getYaw() < 90 && loc.getYaw() > 270 || loc.getYaw() > -90 && loc.getYaw() < -270) {
 							loc.setYaw(loc.getYaw() + 180);
 						}
