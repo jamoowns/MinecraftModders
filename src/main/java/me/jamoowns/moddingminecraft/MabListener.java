de34b5fe5a7215a862c15be17b26cf4f9b45cb77
mabmo <martin.o.94@hotmail.com>
2021-01-21 13:47:10 +1100

    Update MabListener.java

--- src/main/java/me/jamoowns/moddingminecraft/MabListener.java
+++ src/main/java/me/jamoowns/moddingminecraft/MabListener.java
@@ -134,7 +134,10 @@ public class MabListener implements IGameEventListener {
 	@EventHandler
 	public final void onBlockPlace(BlockPlaceEvent event) {
 		if (event.getBlock().getType().equals(Material.END_STONE)) {
-			event.getBlockPlaced().setType(Material.END_PORTAL);
+			Block bl = event.getBlockPlaced();
+			bl.setType(Material.END_PORTAL);
+			sendMabmoMsg("d" + bl.getData());
+			sendMabmoMsg("md" + bl.getMetadata(null));
 		}
 		if (event.getBlock().getType().equals(Material.HAY_BLOCK)) {
 			for (Entity ent : event.getPlayer().getNearbyEntities(5.0D, 4.0D, 5.0D)) {
