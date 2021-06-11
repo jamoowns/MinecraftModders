aad91ef02740a917b8c28440153b5a3cfa6d50af
mabmo <martin.o.94@hotmail.com>
2021-06-11 16:52:19 +1000

    Update Armoury.java

--- src/main/java/me/jamoowns/moddingminecraft/minigames/mgSettings/Armoury.java
+++ src/main/java/me/jamoowns/moddingminecraft/minigames/mgSettings/Armoury.java
@@ -16,6 +16,12 @@ public class Armoury {
 		setFood(FoodLevel);
 	}
 
+	public ArrayList<ItemStack[]> ArmouryTemplate(int Type) {
+		Items = new ArrayList<ItemStack[]>();
+		setTemplate(Type);
+		return Items;
+	}
+
 	public ArrayList<ItemStack[]> getItems() {
 		return Items;
 	}
@@ -97,6 +103,44 @@ public class Armoury {
 		default:
 		}
 
+	}
+
+	private void setTemplate(int type) {
+		switch (type) {
+		case 0:
+			// Archer
+			ItemStack[] temp0 = { (new ItemStack(Material.LEATHER_BOOTS, 1)),
+					(new ItemStack(Material.LEATHER_CHESTPLATE, 1)), (new ItemStack(Material.LEATHER_LEGGINGS, 1)),
+					(new ItemStack(Material.LEATHER_HELMET, 1)), (new ItemStack(Material.BOW, 1)),
+					(new ItemStack(Material.ARROW, 64)), (new ItemStack(Material.ARROW, 64)),
+					(new ItemStack(Material.ARROW, 64)), (new ItemStack(Material.ARROW, 64)),
+					(new ItemStack(Material.BAKED_POTATO, 32)) };
+			Items.add(temp0);
+			break;
+		case 1:
+			// Soldier
+			ItemStack[] temp1 = { (new ItemStack(Material.IRON_SWORD, 1)), (new ItemStack(Material.IRON_BOOTS, 1)),
+					(new ItemStack(Material.IRON_CHESTPLATE, 1)), (new ItemStack(Material.IRON_LEGGINGS, 1)),
+					(new ItemStack(Material.IRON_HELMET, 1)), (new ItemStack(Material.GOLDEN_CARROT, 32)) };
+			Items.add(temp1);
+			break;
+		case 2:
+			// Warrior
+			ItemStack[] temp2 = { (new ItemStack(Material.DIAMOND_AXE, 1)), (new ItemStack(Material.DIAMOND_BOOTS, 1)),
+					(new ItemStack(Material.DIAMOND_CHESTPLATE, 1)), (new ItemStack(Material.DIAMOND_LEGGINGS, 1)),
+					(new ItemStack(Material.DIAMOND_HELMET, 1)), (new ItemStack(Material.COOKED_BEEF, 32)) };
+			Items.add(temp2);
+			break;
+		case 3:
+			// Knight
+			ItemStack[] temp3 = { (new ItemStack(Material.NETHERITE_SWORD, 1)), (new ItemStack(Material.SHIELD, 1)),
+					(new ItemStack(Material.NETHERITE_BOOTS, 1)), (new ItemStack(Material.NETHERITE_CHESTPLATE, 1)),
+					(new ItemStack(Material.NETHERITE_LEGGINGS, 1)), (new ItemStack(Material.NETHERITE_HELMET, 1)),
+					(new ItemStack(Material.BAKED_POTATO, 32)) };
+			Items.add(temp3);
+			break;
+		default:
+		}
 	}
 
 }
