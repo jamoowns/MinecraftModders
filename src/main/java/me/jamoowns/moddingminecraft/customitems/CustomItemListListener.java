f14ddfe275ae6317788f7b05823963e76e1a9cd9
mabmo <martin.o.94@hotmail.com>
2021-06-15 22:42:49 +1000

    Update CustomItemListListener.java

--- src/main/java/me/jamoowns/moddingminecraft/customitems/CustomItemListListener.java
+++ src/main/java/me/jamoowns/moddingminecraft/customitems/CustomItemListListener.java
@@ -712,8 +712,8 @@ public final class CustomItemListListener implements IGameEventListener {
 
 			event.getEntity().getLocation().getWorld().createExplosion(event.getEntity().getLocation(), 18.0F);
 			for (int i = 0; i < 25; i++) {
-				event.getEntity().getLocation().getWorld().createExplosion(event.getEntity().getLocation()
-						.add(-10 + RANDOM.nextInt(20), -16 + RANDOM.nextInt(20), -10 + RANDOM.nextInt(20)), 18.0F);
+				event.getEntity().getLocation().getWorld().createExplosion(event.getEntity().getLocation().add(
+						-10 + RANDOM.nextInt(20), -16 + RANDOM.nextInt(20) + i / 2, -10 + RANDOM.nextInt(20)), 18.0F);
 			}
 		});
 		explosiveBigSnowBallItem.setProjectileLaunchEvent(event -> {
